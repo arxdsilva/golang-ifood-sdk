59b93b6e4d78bfc268d4dc9222b3fbd2823c4a41
Arthur Silva <arxdsilva@gmail.com>
2021-03-20 17:56:02 -0300

    add Unavailabilities

--- services/merchant/merchant.go
+++ services/merchant/merchant.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/arxdsilva/golang-ifood-sdk/adapters"
 	auth "github.com/arxdsilva/golang-ifood-sdk/services/authentication"
@@ -20,6 +21,7 @@ var ErrBadRequest = errors.New("Bad request")
 type (
 	Service interface {
 		ListAll() ([]Merchant, error)
+		Unavailabilities(merchantUUID string) (Unavailabilities, error)
 	}
 
 	Merchant struct {
@@ -40,6 +42,17 @@ type (
 		Postalcode       string `json:"postalCode"`
 	}
 
+	Unavailabilities []Unavailability
+
+	Unavailability struct {
+		ID          string    `json:"id"`
+		Storeid     string    `json:"storeId"`
+		Description string    `json:"description"`
+		Authorid    string    `json:"authorId"`
+		Start       time.Time `json:"start"`
+		End         time.Time `json:"end"`
+	}
+
 	merchantService struct {
 		adapter adapters.Http
 		auth    auth.Service
@@ -51,9 +64,8 @@ func New(adapter adapters.Http, authService auth.Service) *merchantService {
 }
 
 func (m *merchantService) ListAll() (ml []Merchant, err error) {
-	err = m.auth.Validate()
-	if err != nil {
-		glg.Error("[SDK] Merchant auth.Validate: ", err.Error())
+	if err = m.auth.Validate(); err != nil {
+		glg.Error("[SDK] Merchant ListAll auth.Validate: ", err.Error())
 		return
 	}
 	headers := make(map[string]string)
@@ -69,4 +81,25 @@ func (m *merchantService) ListAll() (ml []Merchant, err error) {
 		return
 	}
 	return ml, json.Unmarshal(resp, &ml)
+}
+
+func (m *merchantService) Unavailabilities(merchantUUID string) (mu Unavailabilities, err error) {
+	if err = m.auth.Validate(); err != nil {
+		glg.Error("[SDK] Merchant Unavailabilities auth.Validate: ", err.Error())
+		return
+	}
+	headers := make(map[string]string)
+	headers["Authorization"] = fmt.Sprintf("Bearer %s", m.auth.GetToken())
+	endpoint := fmt.Sprintf("%s/%s/unavailabilities", merchantsV1Endpoint, merchantUUID)
+	resp, status, err := m.adapter.DoRequest(http.MethodGet, endpoint, nil, headers)
+	if err != nil {
+		glg.Error("[SDK] Merchant Unavailabilities adapter.DoRequest error: ", err.Error())
+		return
+	}
+	if status != http.StatusOK {
+		glg.Warn("[SDK] Merchant Unavailabilities status code: ", status)
+		err = ErrBadRequest
+		return
+	}
+	return mu, json.Unmarshal(resp, &mu)
 }
