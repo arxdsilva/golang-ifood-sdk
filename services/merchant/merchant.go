925454104759a68cfd1328c987d135b485e29dd3
Arthur Silva <arxdsilva@gmail.com>
2021-03-21 09:41:24 -0300

    move merchant types to new file

--- services/merchant/merchant.go
+++ services/merchant/merchant.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/arxdsilva/golang-ifood-sdk/adapters"
 	httpadapter "github.com/arxdsilva/golang-ifood-sdk/adapters/http"
@@ -27,78 +26,6 @@ type (
 		CreateUnavailabily(merchantUUID, description string, pauseMinutes int32) (UnavailabilityResponse, error)
 		DeleteUnavailabily(merchantUUID, unavailabilityID string) error
 		Availabily(merchantUUID string) (AvailabilityResponse, error)
-	}
-
-	Merchant struct {
-		ID      string   `json:"id"`
-		Name    string   `json:"name"`
-		Phones  []string `json:"phones"`
-		Address Address  `json:"address"`
-	}
-
-	Address struct {
-		Formattedaddress string `json:"formattedAddress"`
-		Country          string `json:"country"`
-		State            string `json:"state"`
-		City             string `json:"city"`
-		Neighborhood     string `json:"neighborhood"`
-		Streetname       string `json:"streetName"`
-		Streetnumber     string `json:"streetNumber"`
-		Postalcode       string `json:"postalCode"`
-	}
-
-	Unavailabilities []Unavailability
-
-	Unavailability struct {
-		ID          string    `json:"id"`
-		Storeid     string    `json:"storeId"`
-		Description string    `json:"description"`
-		Authorid    string    `json:"authorId"`
-		Start       time.Time `json:"start"`
-		End         time.Time `json:"end"`
-	}
-
-	unavailability struct {
-		Description string `json:"description"`
-		Minutes     int32  `json:"minutes"`
-	}
-
-	UnavailabilityResponse struct {
-		ID          string    `json:"id"`
-		Storeid     string    `json:"storeId"`
-		Description string    `json:"description"`
-		Authorid    string    `json:"authorId"`
-		Start       time.Time `json:"start"`
-		End         time.Time `json:"end"`
-	}
-
-	AvailabilityResponse []Availability
-	Availability         struct {
-		Context    string `json:"context"`
-		Available  bool   `json:"available"`
-		State      string `json:"state"`
-		Reopenable struct {
-			// Identifier interface{} `json:"identifier"`
-			// Type       interface{} `json:"type"`
-			Reopenable bool `json:"reopenable"`
-		} `json:"reopenable"`
-		Validations []struct {
-			ID      string `json:"id"`
-			Code    string `json:"code"`
-			State   string `json:"state"`
-			Message struct {
-				Title       string `json:"title"`
-				Subtitle    string `json:"subtitle"`
-				Description string `json:"description"`
-				Priority    int    `json:"priority"`
-			} `json:"message"`
-		} `json:"validations"`
-		Message struct {
-			Title       string `json:"title"`
-			Subtitle    string `json:"subtitle"`
-			Description string `json:"description"`
-			Priority    int    `json:"priority"`
-		} `json:"message"`
 	}
 
 	merchantService struct {
---
+++ services/merchant/types.go
@@ -0,0 +1,77 @@
+package merchant
+
+import "time"
+
+type (
+	Merchant struct {
+		ID      string   `json:"id"`
+		Name    string   `json:"name"`
+		Phones  []string `json:"phones"`
+		Address Address  `json:"address"`
+	}
+
+	Address struct {
+		Formattedaddress string `json:"formattedAddress"`
+		Country          string `json:"country"`
+		State            string `json:"state"`
+		City             string `json:"city"`
+		Neighborhood     string `json:"neighborhood"`
+		Streetname       string `json:"streetName"`
+		Streetnumber     string `json:"streetNumber"`
+		Postalcode       string `json:"postalCode"`
+	}
+
+	Unavailabilities []Unavailability
+
+	Unavailability struct {
+		ID          string    `json:"id"`
+		Storeid     string    `json:"storeId"`
+		Description string    `json:"description"`
+		Authorid    string    `json:"authorId"`
+		Start       time.Time `json:"start"`
+		End         time.Time `json:"end"`
+	}
+
+	unavailability struct {
+		Description string `json:"description"`
+		Minutes     int32  `json:"minutes"`
+	}
+
+	UnavailabilityResponse struct {
+		ID          string    `json:"id"`
+		Storeid     string    `json:"storeId"`
+		Description string    `json:"description"`
+		Authorid    string    `json:"authorId"`
+		Start       time.Time `json:"start"`
+		End         time.Time `json:"end"`
+	}
+
+	AvailabilityResponse []Availability
+	Availability         struct {
+		Context    string `json:"context"`
+		Available  bool   `json:"available"`
+		State      string `json:"state"`
+		Reopenable struct {
+			// Identifier interface{} `json:"identifier"`
+			// Type       interface{} `json:"type"`
+			Reopenable bool `json:"reopenable"`
+		} `json:"reopenable"`
+		Validations []struct {
+			ID      string `json:"id"`
+			Code    string `json:"code"`
+			State   string `json:"state"`
+			Message struct {
+				Title       string `json:"title"`
+				Subtitle    string `json:"subtitle"`
+				Description string `json:"description"`
+				Priority    int    `json:"priority"`
+			} `json:"message"`
+		} `json:"validations"`
+		Message struct {
+			Title       string `json:"title"`
+			Subtitle    string `json:"subtitle"`
+			Description string `json:"description"`
+			Priority    int    `json:"priority"`
+		} `json:"message"`
+	}
+)
