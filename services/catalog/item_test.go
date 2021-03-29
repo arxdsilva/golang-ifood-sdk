bb6527e3b4e817c3c9de7002086bbc350e0b1ea5
Arthur Silva <arxdsilva@gmail.com>
2021-03-29 08:21:16 -0300

    add test to EditItem

--- services/catalog/item_test.go
+++ services/catalog/item_test.go
@@ -52,3 +52,45 @@ func TestCreateItem_OK(t *testing.T) {
 	assert.Equal(t, float64(10), respCI.Price.Value)
 	assert.Equal(t, 1, respCI.Sequence)
 }
+
+func TestEditItem_OK(t *testing.T) {
+	resp := `{
+		"status": "AVAILABLE",
+		"price": {
+			"value": 10,
+			"originalValue": 0
+		},
+		"externalCode": "string",
+		"sequence": 1,
+		"shifts": [{}]
+	}`
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "/catalog/v2.0/merchants/merchant_id/categories/category_id/products/product_id", r.URL.Path)
+			assert.Equal(t, "Bearer token", r.Header["Authorization"][0])
+			assert.Equal(t, "application/json", r.Header["Content-Type"][0])
+			assert.Equal(t, r.Method, http.MethodPatch)
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, resp)
+		}),
+	)
+	defer ts.Close()
+	am := auth.AuthMock{}
+	am.On("Validate").Once().Return(nil)
+	am.On("GetToken").Once().Return("token")
+	adapter := httpadapter.New(http.DefaultClient, ts.URL)
+	catalogService := New(adapter, &am)
+	assert.NotNil(t, catalogService)
+	ci := CategoryItem{
+		Name:   "id",
+		Status: "AVAILABLE",
+		Price:  Price{Value: 10},
+		Shifts: []Shift{
+			{StartTime: "00:00", EndTime: "23:59", Monday: true},
+		},
+	}
+	respCI, err := catalogService.EditItem("merchant_id", "category_id", "product_id", ci)
+	assert.Nil(t, err)
+	assert.Equal(t, float64(10), respCI.Price.Value)
+	assert.Equal(t, 1, respCI.Sequence)
+}
